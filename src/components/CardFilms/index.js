00448a7ec354c0a30bbcfdfd1a25c509c0cf0ea0
Marcos Vinícius Cavalcante <mvinic2011@gmail.com>
2022-07-12 20:50:54 -0300

    Puxando personagens para retorná-los

--- src/components/CardFilms/index.js
+++ src/components/CardFilms/index.js
@@ -1,9 +1,22 @@
-import { useState } from "react"
+import api from '../../services/api'
+import { useState, useEffect } from "react"
 
-const CardFilms = ({ title, sinopse, atores}) => {
+const CardFilms = ({ title, sinopse, atores }) => {
     
     const [btnVerElenco, setBtnVerElenco] = useState(false)
+    const [personagens, setPersonagens] = useState([])
 
+    useEffect(() => {
+        api.get('people').then(({data}) => {
+            setPersonagens(data.results)
+        });
+
+        console.log(personagens)
+
+    }, ['']);
+
+    //Passar o id do characters do filme, puxar o id dele aqui pra colocar na URL do 'people' e pegar o nome
+    
     function changeButton() {
         if(btnVerElenco === false ) {
             setBtnVerElenco(true)
@@ -22,9 +35,11 @@ const CardFilms = ({ title, sinopse, atores}) => {
             }
 
             {btnVerElenco &&
-            <ul>
-                <li>{atores}</li>
-            </ul>
+                personagens.map((data) => {
+                    <ul>
+                        <li>{data.name}</li>
+                    </ul>
+                })
             }
             
             <button onClick={ changeButton }>
--- src/pages/Filmes/index.js
+++ src/pages/Filmes/index.js
@@ -7,23 +7,22 @@ import CardFilms from "../../components/CardFilms"
 
 const Filmes = ({ children }) => {
 
-    let navigate = useNavigate();
 
-    const [listaFilmes, setListaFilmes] = useState([])
-    const [atores, setAtores] = useState([])
+    const [listaFilmes, setListaFilmes] = useState([]);
     
     useEffect(() => {
         api.get('films').then(({data}) => {
             setListaFilmes(data.results)
         });
         console.log(listaFilmes)
+
     }, ['']);
 
     return(
         <div className="lista-filmes">
             <div className={children}>
                 {listaFilmes?.map((data, index) => (
-                    <CardFilms key={index} title={data.title} sinopse= {data.opening_crawl} atores= {data.characters} />
+                    <CardFilms key={index} title={data.title} sinopse= {data.opening_crawl} atores= {[data.characters]} />
                 ))}
             </div>
         </div>
